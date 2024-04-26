ed80ad6cba3c096495a0975e4a6d6a664f36e668
Tridip Barua Shuvro <157264364+ShuvroScripter@users.noreply.github.com>
2024-04-26 21:50:52 +0600

    Add files via upload
    admin,tests

---
+++ home_service/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from .models import *
+
+# Register your models here.
+admin.site.register(Status)
+admin.site.register(Contact)
+admin.site.register(ID_Card)
+admin.site.register(Order)
+admin.site.register(Service_Category)
+admin.site.register(Service)
+admin.site.register(Customer)
+admin.site.register(Service_Man)
+admin.site.register(Total_Man)
+
